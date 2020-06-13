97c6a135ac189d7aae39186cce7f985ec4678483
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-13 17:52:13 +0300

    Create User Profile & Update User Profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,38 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,abort
 from app.main import main
+from app.models import User
+from .forms import UpdateProfile
+from .. import db
 from app.request import get_quotes
+from flask_login import login_required
 
+#Index Page
 @main.route('/')
 def index():
     quotes = get_quotes()
     print(quotes)
     return render_template('index.html')
 
-@main.route('/profile')
-def profile():
-    return render_template('profile/profile.html')
+#User Profile
+@main.route('/profile/<name>')
+@login_required
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user)
+
+#Update User Profile
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/updateprofile.html',form =form)
