8414bc2f345fff856996a2381265772bc1ebbd9e
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-13 19:16:03 +0300

    Enable User to receive welcome  email on signup

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_mail import Mail
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_uploads import IMAGES, UploadSet,configure_uploads
 
 
-app = Flask(__name__)
-
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+mail = Mail()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app():
-
+    app = Flask(__name__)
     #Create App Configurations
     app.config.from_object(Config)
     app.config['SECRET_KEY'] = 'paper'
@@ -26,5 +27,12 @@ def create_app():
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    #Initialise Flask Extensions
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    configure_uploads(app, photos)
+    mail.init_app(app)
 
     return app
