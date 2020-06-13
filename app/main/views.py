c68ddc7cb35bb255b7cb1a30cbcc12ae543ca495
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-13 17:47:41 +0300

    Create Quotes Query using Quotes API

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 from flask import render_template,redirect,url_for
 from app.main import main
+from app.request import get_quotes
 
 @main.route('/')
 def index():
+    quotes = get_quotes()
+    print(quotes)
     return render_template('index.html')
 
 @main.route('/profile')
