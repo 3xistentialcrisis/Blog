fa455f32f3197a5dd75c9408d067987b418ca5cc
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-13 21:46:12 +0300

    Create Comments model

--- app/models.py
+++ app/models.py
@@ -36,6 +36,7 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     hashed_password = db.Column(db.String(255), nullable=False)
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
 
     @property
     def set_password(self):
@@ -67,6 +68,7 @@ class Blog(db.Model):
     content = db.Column(db.Text(),nullable=False)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
 
     def save(self):
         db.session.add(self)
@@ -83,3 +85,24 @@ class Blog(db.Model):
 
     def __repr__(self):
         return f'Blog {self.title}'
+
+#Blog Comments
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    post_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
