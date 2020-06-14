f1cf55ac3def4f6d09c7b2c3e12ce1735f1d46bd
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-14 15:08:13 +0300

    Add #showform & #submit click functions

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,8 +3,19 @@ $(document).ready(function(){
       $("#updates").slideDown('1500').hide('1000');
       $("#imgform").show('1500');
     });
+
     $("#submit").click(function(){
       $("#imgform").slideUp('1500');
       $("#updates").slideDown('1500');
     });
+
+    $("#showform").click(function(){
+       $("#profileupdate").slideDown('2500');
+     });
+
+    $("#submit").click(function(){
+       $("#profileupdate").slideUp('1500');
+     });
+
   });
+
